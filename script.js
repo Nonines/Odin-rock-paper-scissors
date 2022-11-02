307827249ace39d4a70ebaf2dcbb7f38ff76ff04
Banky <kingmorty@protonmail.com>
2022-11-02 15:14:29 +0100

    Add clearer messages for each game round

--- script.js
+++ script.js
@@ -37,6 +37,39 @@ function playRound(playerSelection) {
         winner = "com";
     }
 
-    // showResults(playerSelection, comSelection, winner);
+    showResults(playerSelection, comSelection, winner);
     // displayScore(winner);
+}
+
+const roundContainer = document.querySelector("#round-container");
+
+const roundInfo = document.createElement("p");
+const roundWinInfo = document.createElement("p");
+roundContainer.appendChild(roundInfo);
+roundContainer.appendChild(roundWinInfo);
+
+function showResults(playerSelection, comSelection, winner) {
+    roundInfo.textContent = `You chose ${playerSelection.toUpperCase()}. COM chose ${comSelection.toUpperCase()}.`;
+
+    let winningChoice, losingChoice;
+
+    switch(winner) {
+        case "player":
+            roundWinInfo.textContent = "You win!";
+            winningChoice = playerSelection;
+            losingChoice = comSelection;
+            break;
+
+        case "com":
+            roundWinInfo.textContent = "COM wins!";
+            winningChoice = comSelection;
+            losingChoice = playerSelection;
+            break;
+
+        case "draw":
+            roundWinInfo.textContent = "It's a tie. Try again!";
+            return;
+    }
+
+    roundWinInfo.textContent += ` ${winningChoice.toUpperCase()} beats ${losingChoice}.`;
 }
